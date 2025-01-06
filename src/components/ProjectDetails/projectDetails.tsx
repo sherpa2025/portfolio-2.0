69de8d9b3c10a9f715377cd6127f3ecbe32d9a60
Nima Sherpa <79430796+sherpa2025@users.noreply.github.com>
2025-01-06 15:56:29 -0700

    Update projectDetails.tsx

--- src/components/ProjectDetails/projectDetails.tsx
+++ src/components/ProjectDetails/projectDetails.tsx
@@ -9,6 +9,7 @@ import {
   Text,
 } from "@chakra-ui/react";
 import Github from "../../assets/github.svg";
+import Demo from "../../assets/demo.png";
 import { ProjectDrawerProps } from "../../types";
 import Close from "../../assets/close.svg";
 import style from "./details.module.css";
@@ -28,31 +29,41 @@ const ProjectDetails: React.FC<ProjectDrawerProps> = ({
           <Image src={Close} onClick={onClose} />
         </DrawerHeader>
         <DrawerBody>
-          <div className={style["project-image"]}>
-            <Image
-              src={project.imageSrc}
-              alt={project.title}
-              borderRadius="lg"
-            />
-          </div>
-          <Text>
-            <strong>Description: </strong> {project.description}
-          </Text>
-          <Text mt="4" fontWeight="bold">
-            Tools:
-          </Text>
-          <ul>
-            {project.tools.map((tool, index) => (
-              <li key={index}>{tool}</li>
-            ))}
-          </ul>
-          <hr className={style.line} />
-          <div className={style.image}>
-            <a href={project.githubLink} target="_blank">
-              <Image src={Github} alt="GitHub" />
-            </a>
-          </div>
-        </DrawerBody>
+  <div className={style["project-image"]}>
+    <Image
+      src={project.imageSrc}
+      alt={project.title}
+      borderRadius="lg"
+    />
+  </div>
+  <Text>
+    <strong>Description: </strong> {project.description}
+  </Text>
+  <Text mt="4" fontWeight="bold">
+    Tools:
+  </Text>
+  <ul>
+    {project.tools.map((tool, index) => (
+      <li key={index}>{tool}</li>
+    ))}
+  </ul>
+  <hr className={style.line} />
+
+  {/* Conditionally render the GitHub icon link */}
+  {project.githubLink && (
+    <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
+      <Image src={Github} alt="GitHub" className={style["footer-image"]} />
+    </a>
+  )}
+
+  {/* Conditionally render the Live Demo icon link */}
+  {project.liveDemo && (
+    <a href={project.liveDemo} target="_blank" rel="noopener noreferrer">
+      <Image src={Demo} alt="Live Demo" className={style["footer-image"]} />
+    </a>
+  )}
+</DrawerBody>
+
       </DrawerContent>
     </Drawer>
   );
